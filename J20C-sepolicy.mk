f564bf0a56ab6590cec2b1ba9e45dd9ee885aec8
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-03-26 17:14:00 +0000

    J20C-sepolicy: nfc: Add secure_element denials
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I663367086a63284f7378b7829a69611c0b006750

--- J20C-sepolicy.mk
+++ J20C-sepolicy.mk
@@ -8,6 +8,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/xiaomi/J20C-sepolicy/ir/vendor \
     device/xiaomi/J20C-sepolicy/fingerprint/vendor \
     device/xiaomi/J20C-sepolicy/leds/vendor \
+    device/xiaomi/J20C-sepolicy/nfc/vendor \
     device/xiaomi/J20C-sepolicy/qcom-extra/vendor \
     device/xiaomi/J20C-sepolicy/thermal/vendor
 
