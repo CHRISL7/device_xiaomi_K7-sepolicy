ce4204e819ab308fc0eddd29ac6ad83d11feee56
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-03-28 17:55:28 +0000

    J20C-sepolicy: Commonize some sepolicy [2/2] Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com> Change-Id: I5d102e131942a7d72dddecb0e83b1201a75ba0bd

--- J20C-sepolicy.mk
+++ J20C-sepolicy.mk
@@ -2,7 +2,9 @@ ifeq ($(J20C_SEPOLICY_INCLUDED),)
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/xiaomi/J20C-sepolicy/audio/vendor \
+    device/xiaomi/J20C-sepolicy/battery/vendor \
     device/xiaomi/J20C-sepolicy/batterysecret/vendor \
+    device/xiaomi/J20C-sepolicy/common/vendor \
     device/xiaomi/J20C-sepolicy/power/vendor \
     device/xiaomi/J20C-sepolicy/camera/vendor \
     device/xiaomi/J20C-sepolicy/ir/vendor \
@@ -11,12 +13,14 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/xiaomi/J20C-sepolicy/nfc/vendor \
     device/xiaomi/J20C-sepolicy/perf/vendor \
     device/xiaomi/J20C-sepolicy/qcom-extra/vendor \
+    device/xiaomi/J20C-sepolicy/telephony/vendor \
     device/xiaomi/J20C-sepolicy/thermal/vendor \
     device/xiaomi/J20C-sepolicy/usb/vendor
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/xiaomi/J20C-sepolicy/audio/private \
-    device/xiaomi/J20C-sepolicy/camera/private
+    device/xiaomi/J20C-sepolicy/camera/private \
+    device/xiaomi/J20C-sepolicy/common/private
 
 J20C_SEPOLICY_INCLUDED := true
 endif
